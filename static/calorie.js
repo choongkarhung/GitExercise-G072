7640a7053b25b4f34453269a0070196e9cc10d25
Kar Hung <choongkarhung@gmail.com>
2026-05-24 16:08:18 +0800

    Calorie javascript started.

---
+++ static/calorie.js
@@ -0,0 +1,5 @@
+// ── STATE ──
+let selectedGender   = 'male';
+let selectedActivity = 1.2;
+let selectedSpeed    = 250;
+let lastResult       = null;   // holds the last calculated result for saving
